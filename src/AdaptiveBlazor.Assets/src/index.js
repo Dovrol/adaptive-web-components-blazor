fef88eb85bd7769bce6e60177b7b4be83382a0e5
Dovrol <wiktor.kubis@gmail.com>
2024-01-28 00:04:46 +0100

    Added default registration of components.

--- src/AdaptiveBlazor.Assets/src/index.js
+++ src/AdaptiveBlazor.Assets/src/index.js
@@ -1,1 +1,7 @@
-export * from '@adaptive-web/adaptive-web-components'
+import { AdaptiveDesignSystem } from "@adaptive-web/adaptive-web-components";
+import { AllComponents } from "@adaptive-web/adaptive-web-components/all-components";
+import { DesignToken } from "@microsoft/fast-foundation";
+
+// This must be called during initialization for the Design Tokens to be setup so the component styling is applied.
+DesignToken.registerDefaultStyleTarget();
+AdaptiveDesignSystem.defineComponents(AllComponents);

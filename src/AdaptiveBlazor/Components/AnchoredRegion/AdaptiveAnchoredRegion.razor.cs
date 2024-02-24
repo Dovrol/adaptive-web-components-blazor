cd3f8b883c0a5543a9a296e4d8bc3c9035a93cff
Dovrol <wiktor.kubis@gmail.com>
2024-02-24 18:30:54 +0100

    Added stories for anchored region component

--- src/AdaptiveBlazor/Components/AnchoredRegion/AdaptiveAnchoredRegion.razor.cs
+++ src/AdaptiveBlazor/Components/AnchoredRegion/AdaptiveAnchoredRegion.razor.cs
@@ -4,6 +4,7 @@ namespace AdaptiveBlazor.Components;
 
 /// <summary>
 /// An anchored region is a container component which enables authors to create layouts where the contents of the anchored region can be positioned relative to another "anchor" element. Additionally, the anchored region can react to the available space between the anchor and a parent "viewport" element such that the region is placed on the side of the anchor with the most available space, or even resize itself based on that space.
+/// For more information <see href="https://github.com/microsoft/fast/packages/web-components/fast-foundation/src/anchored-region/anchored-region.spec.md">Microsoft FAST Docs</see>
 /// </summary>
 public partial class AdaptiveAnchoredRegion
 {
